0e3cb808cabb301f6e1bb5d25f9d4d8e2192500b
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2023-02-21 17:04:05 +0530

    Feat : Sidebar ✅

---
+++ Readme.md
@@ -0,0 +1,7 @@
+## Notes App
+
+-   SideBar
+    -   [x] Add a note
+    -   [x] Get Current Note
+    -   [x] Delete a Note
+    -   [x] Highlight Selected Note
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,81 @@
-import React from "react";
+import { nanoid } from "nanoid";
+import React, { useEffect, useState } from "react";
 
 function Sidebar() {
-	return <aside className="sidebar">Sidebar</aside>;
+	const [notes, setNotes] = useState([]);
+	const [currentNoteIndex, setCurrentNoteIndex] =
+		useState(0);
+
+	const addNote = () => {
+		let noteObj = {
+			id: nanoid(),
+			body: "Start Writing here",
+			title: `New Note ${notes.length + 1}`,
+		};
+
+		setNotes((oldNotes) => [...oldNotes, noteObj]);
+	};
+
+	const getCurrentNote = () => {
+		let currentNote = notes.find(
+			(note) => note.id === currentNoteIndex
+		);
+
+		console.log(currentNote);
+	};
+
+	const deleteNote = (id) => {
+		setNotes((oldNotes) => {
+			return oldNotes.filter(
+				(note) => note.id !== id
+			);
+		});
+	};
+
+	return (
+		<aside className="sidebar">
+			<div className="sidebar-title">
+				{" "}
+				Add a note
+				<span className="notes-count">
+					{notes.length}
+				</span>
+			</div>
+			<button onClick={addNote}>+</button>
+			<ul className="notes">
+				{notes &&
+					notes.map((note) => {
+						let isSelected =
+							currentNoteIndex === note.id;
+						return (
+							<li
+								className={`note ${
+									isSelected
+										? "selected-note"
+										: ""
+								}`}
+								key={note.id}
+								onClick={() =>
+									setCurrentNoteIndex(
+										note.id
+									)
+								}
+							>
+								{note.title}
+								<span
+									className="note-delete"
+									onClick={() =>
+										deleteNote(note.id)
+									}
+								>
+									❌
+								</span>
+							</li>
+						);
+					})}
+			</ul>
+		</aside>
+	);
 }
 
 export default Sidebar;
